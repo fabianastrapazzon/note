6f3a4569bcf5076a55762662b79708cd045704da
fabiananewtech <fabiana.strapazzon@newtechs.com.br>
2025-04-10 07:07:27 -0300

    Aulas: 16, 17, 18, 19, 20

--- src/note/note.controller.ts
+++ src/note/note.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller('note') //decorator de classe
@@ -13,5 +21,26 @@ export class NoteController {
     console.log(request);
 
     return `Essa rota retorna UM recado ID ${request}`;
+  }
+
+  @Post('create')
+  create(@Body('nova chave') request: any) {
+    console.log(request);
+
+    //return `Essa rota cria recados`;
+    return request;
+  }
+
+  @Patch('update/:id')
+  update(@Param('id') id: Request, @Body() body: any) {
+    return {
+      id,
+      ...body,
+    };
+  }
+
+  @Delete('remove/:id')
+  remove(@Param('id') id: Request) {
+    return `O recado de ID ${id} foi deletado`;
   }
 }
